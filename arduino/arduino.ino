9b54bf6e1e7b96ba8cb1ef71c5a66365f91e8bb8
papitflepa <papitflepa8@gmail.com>
2024-04-06 18:10:06 +0300

    обновлённые изменения

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,7 +5,7 @@ Servo myservo;
 void setup() {
   // Начинаем работу с Serial портом на скорости 9600 бит/с
   Serial.begin(9600);
-  myservo.attach(7);
+  myservo.attach(6);
 
   //получаем значение сервопривода в градусах
   int pos = myservo.read();
@@ -13,7 +13,7 @@ void setup() {
   delay(300);
   
 //Возвращаем плечи вешалки в начальное положение
-  for (int i = pos; i <= 100; i++) {   
+  for (int i = pos; i <= 200; i++) {   
     myservo.write(i);
     delay(150);
   }
@@ -25,12 +25,12 @@ void loop() {
   // myservo.write(100);
 
   // Проверяем, есть ли данные из Serial порта
-  if (Serial.available() == 1) {
+  if (Serial.available() > 0) {
     // Читаем полученные данные и сохраняем их в переменной
     String receivedData = Serial.readString();
 
 //Складываем плечики вешалки
-    for (int i = 100; i <= 170; i++) {   
+    for (int i = 200; i <= 100; i++) {   
       myservo.write(i);
       delay(15);
     }
--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -3,7 +3,7 @@ import serial.tools.list_ports
 from paho.mqtt.subscribe import simple
 
 #Связь с ардуино уно
-arduino=serial.Serial('COM3',9600)
+arduino=serial.Serial('COM6',9600)
 
 #Связь пайтона с управлением
 message=simple('maker/#',hostname='95.163.230.191',port=1883,auth={'username':'maker','password':'LABmaker123'})
@@ -13,19 +13,7 @@ print(message.payload.decode())
 
 #Если пришло сообщение, отправляем сигнал к ардуино
 if message.payload.decode() == '1':
-    print("футболка 1")
-    arduino.write(b'message.payload.encode()')
-    time.sleep(1)
-
-    # Считываем строку с Arduino
-    data = arduino.readline().decode('utf-8').strip()
-    # Выводим текущие показания
-    print(data)
-    print('футболка получена')
-
-#Если пришло сообщение, отправляем сигнал к ардуино
-if message.payload.decode() == '2':
-    print("футболка 2")
+    print("!!!!")
     arduino.write(b'message.payload.encode()')
     time.sleep(1)
 
